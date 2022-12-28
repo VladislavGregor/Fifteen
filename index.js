ad730bd1d7786906d3336266738e21d9241f07d0
VladislavGregor <114413651+VladislavGregor@users.noreply.github.com>
2022-12-28 21:29:10 +0300

    Cleared the cod from all the map functions, and did it with for-functions

--- index.js
+++ index.js
@@ -13,13 +13,36 @@ if (itemNodes.length != 16) {
 
 itemNodes[countItems-1].style.display = 'none';
 
+//let matrix = GetMatrix(
+ // itemNodes.map((item) => Number(item.dataset.matrixId))
+//);
+
+let arr = [];
+
+for(var i=0 ; i<16; i++){
+   arr[i] = i+1;
+}
+console.log(arr);
+
 let matrix = GetMatrix(
-    itemNodes.map((item) => Number(item.dataset.matrixId))
+    arr
 );
+
+console.log(matrix);
+
+const l = matrix.length;
+
+console.log(l);
+	
+
+
+
 
 setPositionItems(matrix);
 
-console.log(matrix);
+
+
+
 
 function GetMatrix(arr) {
 
@@ -46,22 +69,26 @@ function GetMatrix(arr) {
 
 document.getElementById('shuffle').addEventListener('click', () => {
     const shuffledArray = shuffleArray(matrix.flat());
+    console.log(shuffledArray);
     matrix = GetMatrix(shuffledArray);
     setPositionItems(matrix);
 })
 
 
 function setPositionItems (matrix) {
-    for ( let y = 0 ; y < matrix.length; y++) {
-        for (let x = 0; x < matrix.length; x++) {
+    len = matrix.length;
+    for ( let y = 0 ; y < len; y++) {
+        for (let x = 0; x < len; x++) {
             const value = matrix [y][x];
+            console.log(value);
             const node = itemNodes[value - 1];
-            setNodeStyles(node, x, y)
+            console.log(node);
+            setNodeStyles (node, x, y);
         }
     }
 }
 
-function setNodeStyles (node, x ,y) {
+function setNodeStyles ( node , x ,y) {
     const shiftPs = 100;
     node.style.transform = `translate3D(${x * shiftPs}%, ${y* shiftPs}%, 0) `
 }
